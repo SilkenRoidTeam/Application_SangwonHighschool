f35a6fb0175574d40de23b603a6311319389540d
silkenroidteam <anstlr6935@gmail.com>
2013-05-12 15:49:36 +0900

    Add Many Functions
    Add Many App Functions

--- src/com/sdf/swhs/MainActivity.java
+++ src/com/sdf/swhs/MainActivity.java
@@ -1,21 +1,25 @@
 package com.sdf.swhs;
 
-import android.content.*;
 import android.net.*;
 import android.view.View.*;
 import android.widget.*;
+import android.annotation.SuppressLint;
 import android.app.Activity; 
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle; 
 import android.os.Handler;
 import android.os.Message;
 import android.view.View;
 
 
+	@SuppressLint("HandlerLeak")
 	public class MainActivity extends Activity
 	{		
+		Handler handler;
+	    ImageView splashImage;
 		
 		boolean m_close_flag = false;
         Handler m_close_handler = new Handler() {
@@ -26,7 +30,7 @@ import android.view.View;
         public void onBackPressed () 
         {
             if(m_close_flag == false) { 
-                Toast.makeText(this, "Back 키를 한번 더 누르면 종료됩니다.", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "'뒤로' 키를 한번 더 누르면 종료됩니다.", Toast.LENGTH_LONG).show();
                 m_close_flag = true;
                 m_close_handler.sendEmptyMessageDelayed(0, 3000);
             } else { 
@@ -45,6 +49,7 @@ import android.view.View;
 	{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        startActivity(new Intent(this,SplashActivity.class));
         Button Button1 = (Button)findViewById(R.id.Button1); 
         Button1.setOnClickListener(new View.OnClickListener(){ 
 		 public void onClick(View v){ 		
@@ -59,7 +64,7 @@ import android.view.View;
 				public void onClick(View v){ 	
 				            AlertDialog.Builder alert = new AlertDialog.Builder(context);
 				            alert.setTitle("정보");
-				            alert.setMessage("체인지로그\n- 디자인 변경\n- Android 2.0.1 이상 지원\n- 시설 배치도 이미지 이동,확대&축소 가능\n- 앱 구조 최적화\n- 아이콘 변경\n- 종료 알림 추가\n\n개발자 : 10501 김문식\nVersion : Preview-FINAL"); 
+				            alert.setMessage("체인지로그\n- 디자인 변경\n- Android 2.0.1 이상 지원\n- 시설 배치도 이미지 이동,확대&축소 가능\n- 앱 구조 최적화\n- 아이콘 변경\n- 종료 알림 추가\n- 로딩 이미지 추가\n\n개발자 : 10501 김문식\nVersion : Preview-FINAL"); 
 				 
 				            alert.setNegativeButton("확인", new DialogInterface.OnClickListener() {
 				            public void onClick(DialogInterface dialog, int whichButton) { 
@@ -109,7 +114,7 @@ import android.view.View;
 			{ Intent i=new Intent(MainActivity.this,classhp.class);
 			startActivity(i);
 	    	}});
-			
+		
 }
 
 	}
